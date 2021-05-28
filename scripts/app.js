5ba1a210440861a7cfe31925bc0fc63e6dc012c0
Victoria Olanipekun <queentoria1@gmail.com>
2021-05-28 12:44:20 +0100

    PROJECT-1-HTML_CSS_ongoing

---
+++ scripts/app.js
@@ -0,0 +1,35 @@
+function init() {
+  console.log('js up and running')
+
+
+
+
+}
+
+window.addEventListener('DOMContentLoaded', init)
+
+
+
+// HTML
+// Score, start, buttons - using the function and if statement (should I make the button in the screen or let the game work with the user’s keyboard and then how do I link them together)
+
+// CSS
+// Style as needed - grid height and width, grid wrapper, the snake, the apple
+
+// Javascript
+// Define my variables such as the grid, score, width, snakeCurrentSize,  using document.querySelector to  get the various classes that would be functional e.g the grid, score container 
+// Add eventlistner
+
+// The Snake - divs with the same color
+
+// The snake gets longer as it feeds - the use of the current size/array 
+
+// The snake container/house - grids 8X8 , looping to 64  and these grids will be stored in the snake container 
+
+// Snake movement - set direction for snakes, set interval for the time it will take for the snake to move around and the if statement to condition the user to loose if the container edge is touched I.e passed
+
+// Movement direction by the user - use event.keyCode, adding the set width or subtracting it, then set an eventListener 
+
+// Snake food - using random number 
+// Snake feeding - the divs without the snake is where the apple function pops up - in a conditional statement within a function
+
